75297eb2b9d8e924a25cba67accafcc8bf3302de
nTraum <philipp.press@googlemail.com>
2013-12-13 15:15:29 +0100

    include branch in irc message

--- lib/web.rb
+++ lib/web.rb
@@ -12,10 +12,11 @@ set :bind, config['web']['bind']
 post '/commit' do
   redis = Redis.new(:host => config['redis']['host'], :port => config['redis']['port'])
   json = MultiJson.load request.body.read
+  branch = json['ref'].split('/').last
   json['commits'].each do |commit|
         url = Googl.shorten(commit['url']).short_url
         commit_message = commit['message'].gsub(/\n/," ")
-        irc_message = "[#{json['repository']['name'].capitalize}] #{commit['author']['name']} | #{commit_message} | #{url}"
+        irc_message = "[#{json['repository']['name'].capitalize}(#{branch})] #{commit['author']['name']} | #{commit_message} | #{url}"
         redis.rpush("#{config['redis']['namespace']}:messages", irc_message)
         redis.quit
     end
