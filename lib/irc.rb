2ea26fd74f04979c2cf587e83a965359542fee3d
Tim Vos <TimTims@users.noreply.github.com>
2014-09-07 11:40:06 +0100

    Update irc.rb

--- lib/irc.rb
+++ lib/irc.rb
@@ -23,6 +23,7 @@ bot = Cinch::Bot.new do
   configure do |c|
     c.server = $config['irc']['server']
     c.port = $config['irc']['port']
+    c.password = $config['irc']['server_password']
     c.channels = [[$config['irc']['channel'], $config['irc']['channel_password']].compact.join(' ')]
     c.nick = $config['irc']['nickname']
     c.realname = $config['irc']['nickname']
