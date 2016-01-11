c088517fff7dd385b4966df108baca921007d937
Your Name <you@example.com>
2016-01-11 22:58:31 +0100

    add support for issue/note/merge_request/tag_push

--- lib/message_formatter.rb
+++ lib/message_formatter.rb
@@ -5,17 +5,53 @@ class MessageFormatter
   def self.messages(json)
     msgs = []
     info = MultiJson.load(json)
-    branch = info['ref'].split('/').last
-    info['commits'].each do |ci|
-      url = self.short_url(ci['url'])
-      ci_title = ci['message'].lines.first.chomp
-      msg = "[#{info['repository']['name'].capitalize}(#{branch})] #{ci['author']['name']} | #{ci_title} | #{url}"
-      msgs << msg
+
+
+    if info['object_kind'] == 'push'
+        branch = info['ref'].split('/').last
+        info['commits'].each do |ci|
+            url = ci['url']
+            ci_title = ci['message'].lines.first.chomp
+            msg = "[#{info['object_kind']} -----#{info['repository']['name'].capitalize}(#{branch})] #{ci['author']['name']} | #{ci_title} | #{url}"
+            msgs << msg
+        end
+    elsif info['object_kind'] == 'tag_push'
+
+        msg = "#{info['object_kind']} TAG PUSH MESSAGE"
+        msgs << msg    
+
+    elsif info['object_kind'] == 'issue'
+            
+        if info['object_attributes']['created_at'] == info['object_attributes']['updated_at']
+            new = "New issue"
+        else
+            new = "Issue updated"
+        end
+        
+        author = info['user']['username']
+        title = info['object_attributes']['title']
+        url = info['object_attributes']['url']
+        msg = "[#{new}] by #{author} | #{title} | #{url}"
+        msgs << msg
+          
+    elsif info['object_kind'] == 'note'
+
+        noteable = info['object_attributes']['object_attributes']
+        new = "New note on #{noteable}"
+        msg = "[#{new}] #{info['object_attributes']['note']} | #{info['object_attributes']['url']}"
+        msgs << msg    
+
+    elsif info['object_kind'] == 'merge_request'
+            msg = "#{info['object_kind']} MESSAGE"
+            msgs << msg    
     end
+        
     return msgs
   end
 
-  def self.short_url(url)
-    Googl.shorten(url).short_url
-  end
+
+#   def self.short_url(url)
+#     Googl.shorten(url).short_url
+#   end
+
 end
