0871efbb57204122a1266bd37562b805cab7a2c2
Your Name <you@example.com>
2016-01-12 00:40:33 +0100

    add global config to suppress notifications

--- lib/message_formatter.rb
+++ lib/message_formatter.rb
@@ -3,19 +3,21 @@ require 'googl'
 
 class MessageFormatter
   def self.messages(json)
+
+    config = YAML.load_file(File.join('config', 'config.yml'))
+
     msgs = []
     info = MultiJson.load(json)
 
-
-    if info['object_kind'] == 'push'
+    if info['object_kind'] == 'push' && config['msg']['push'] == true
         branch = info['ref'].split('/').last
         info['commits'].each do |ci|
             url = ci['url']
             ci_title = ci['message'].lines.first.chomp
-            msg = "[#{info['object_kind']} -----#{info['repository']['name'].capitalize}(#{branch})] #{ci['author']['name']} | #{ci_title} | #{url}"
+            msg = "[#{info['repository']['name'].capitalize}(#{branch})] #{ci['author']['name']} | #{ci_title} | #{url}"
             msgs << msg
         end
-    elsif info['object_kind'] == 'tag_push'
+    elsif info['object_kind'] == 'tag_push' && config['msg']['tag_push'] == true
 
         if info['before'] == '0000000000000000000000000000000000000000'
             new = "New tag"
@@ -26,7 +28,7 @@ class MessageFormatter
         msg = "[#{new}] #{info['ref']} by #{info['repository']['name']}"
         msgs << msg    
 
-    elsif info['object_kind'] == 'issue'
+    elsif info['object_kind'] == 'issue' && config['msg']['issue'] == true
             
         if info['object_attributes']['created_at'] == info['object_attributes']['updated_at']
             new = "New issue"
@@ -40,11 +42,12 @@ class MessageFormatter
         msg = "[#{new}] by #{author} | #{title} | #{url}"
         msgs << msg
           
-    elsif info['object_kind'] == 'note'
+    elsif info['object_kind'] == 'note' && config['msg']['note'] == true
 
-        noteable = info['object_attributes']['object_attributes']
-        new = "New note on #{noteable}"
-        msg = "[#{new}] #{info['object_attributes']['note']} | #{info['object_attributes']['url']}"
+        noteable = info['object_attributes']['noteable_type']
+        username = info['user']['username'] 
+        new = "New note on #{noteable} by #{username}"
+        msg = "[#{new}] #{info['object_attributes']['url']}"
         msgs << msg    
 
     end
